3462fa634f3047e33ef2b332e32da0aba8bfb665
risharma1 <risharma04@gmail.com>
2019-05-17 20:47:00 +0530

    JSON complete

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,17 +1,36 @@
-//task is to take an object ,convert it to string/bytes,
-//then write it to a file, read it back and then convert again to the object
+// //task is to take an object ,convert it to string/bytes,
+// //then write it to a file, read it back and then convert again to the object
 
-const book = {
-    title: "Ego isthe enemy",
-    author: "Ryan Holiday",
-}
+const fs = require('fs')
 
-//method 1
-const bookJson = JSON.stringify(book)
-console.log(book)
-console.log(bookJson)
-/**{ title: 'Ego isthe enemy', author: 'Ryan Holiday' }
-{"title":"Ego isthe enemy","author":"Ryan Holiday"} */
+// const book = {
+//     title: "Ego is the enemy",
+//     author: "Ryan Holiday",
+// }
 
-const restoredObject = JSON.parse(bookJson)
-console.log(restoredObject.author)
+// //method 1
+// const bookJson = JSON.stringify(book)
+// // console.log(book)
+// // console.log(bookJson)
+// /**{ title: 'Ego isthe enemy', author: 'Ryan Holiday' }
+// {"title":"Ego isthe enemy","author":"Ryan Holiday"} */
+
+// // const restoredObject = JSON.parse(bookJson)
+// // console.log(restoredObject.author)
+
+// fs.writeFileSync('1-json.json',bookJson)
+
+// //file contents are read as bytes
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const readObject = JSON.parse(dataBuffer)
+// console.log(dataBuffer)
+// console.log(dataBuffer.toString())
+// console.log(readObject.author)
+
+const readObject = JSON.parse(fs.readFileSync('1-json.json'))
+
+readObject.name = 'Rishabh'
+readObject.age = 28
+readObject.planet = 'Mars'
+
+fs.writeFileSync('1-json.json', JSON.stringify(readObject))
