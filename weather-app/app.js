ce048457250d9e3b4020d854a2c59abe28ffc2bf
risharma1 <risharma04@gmail.com>
2019-05-23 00:50:13 +0530

    Studying async behavior and Raising an Http reuqest using request npm package

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,15 @@
-console.log('Starting')
+const request = require('request')
 
-//basic async behavior
-setTimeout( () => {
-    console.log('2 second timer')
-}, 2000)
+const urlWithKey = 'https://api.darksky.net/forecast/c6f3879289c51ce92d1411e9d8ec1c0b/'
 
-setTimeout(()=>{
-    console.log('0 second timer')
-}, 0) //comesafter the following sync commandds even if its just 0 sec wait
+const lat = '37.8267'
+const long = '-122.4233'
 
-
-console.log('Stopping')
+request({
+    url: urlWithKey+lat+','+long,
+}, (error, response) => {
+    //console.log(response)
+    debugger
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+})
