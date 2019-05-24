387c2b7de8012c3939f57bc818484d3077e4e4d3
risharma1 <risharma04@gmail.com>
2019-05-24 19:55:38 +0530

    sending get params in url

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,10 +5,13 @@ const urlWithKey = 'https://api.darksky.net/forecast/c6f3879289c51ce92d1411e9d8e
 const lat = '37.8267'
 const long = '-122.4233'
 
+const optionalParam = '?units=si'
+
 request({
-    url: urlWithKey+lat+','+long,
+    url: urlWithKey+lat+','+long+ optionalParam,
     json: true, //using this our reuqest package will parse the response assuming its json
 }, (error, response) => {
     console.log('It is currently %s degrees', response.body.currently.temperature)
     console.log('There is %s \% chance of rain', response.body.currently.precipProbability)
+    console.log(response.body.currently)
 })
