7ad779e56553f5b9fb9e10c0e2ee6f4185dd6f39
risharma1 <risharma04@gmail.com>
2019-06-10 22:22:47 +0530

    Using handle bars templates to serve dynamic content and setting routes to those handlebar templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -16,6 +16,35 @@ without setting up explicit app.get routes
 */
 app.use(express.static(publicDirectoryPath))
 
+//app.set allows us to set a value for given Express setting
+//here we are setting our Dynamic Templating Engine handlebars
+//and Express starts expecting "views"
+app.set('view engine', 'hbs')
+
+//setting route for serving hbs
+app.get('', (req, res)=>{
+   //here we use res.render() method to serve views or hbs templates
+   res.render('index', { //knows to look in "views" folder
+   title: 'Weather',
+   name: "Rishabh"
+   })
+})
+
+app.get('/about', (req, res)=>{
+   res.render('about', {
+      title: 'About Me',
+      name: 'Rishabh'
+   })
+})
+
+
+app.get('/help', (req, res)=>{
+   res.render('help', {
+      helptext: 'Help Page text',
+      name: 'Rishabh'
+   })
+})
+
 /** Configuring routesfor our server
  * app.com - root
  * app.com/help - subroute
