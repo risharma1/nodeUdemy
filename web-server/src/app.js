09c6804198e75020bd6d74479447501a6a22c546
risharma1 <risharma04@gmail.com>
2019-06-13 00:37:12 +0530

    gettingquery params from endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -62,10 +62,25 @@ app.get('/help', (req, res) => {
  */
 
  app.get('/weather', (req,res) => {
+   if (!req.query.address) {
+      return res.send({
+          error: 'You must provide an address'
+       })
+    }
     res.send({
-        forecast: 'its raining',
-        location: 'Bengaluru'
+        address: req.query.address
     })
+ })
+
+ app.get('/products', (req, res) => {
+    if (!req.query.search) {
+      return res.send({
+          error: 'You must provide a search term.'
+       })
+    }
+   res.send({
+      products:[]
+   })
  })
 
  //if we want to customize the subroute pages
