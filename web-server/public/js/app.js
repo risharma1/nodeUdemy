2ace3f186661b697250178aeddfe0477a1c5af80
risharma1 <risharma04@gmail.com>
2019-06-18 23:30:12 +0530

    UI web page to host weather queries using css, client side js, fetch, js selectors

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,16 +1,45 @@
 console.log("Client side js is loaded")
 
-//client side js
-//async io
-//a browser provided feature
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+// //client side js
+// //async io
+// //a browser provided feature
+// fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+//     response.json().then(({error, fullname, forecast} = {}) => {
+//         //runs when the json has arrived and parsed
+//         if(error){
+//             console.log(error)
+//         }else{
+//             console.log(fullname)
+//             console.log(forecast)
+//         }
+//     })
+// })
+
+const weatherForm = document.querySelector('form')
+const searchBox = document.querySelector('input')
+
+//selecting paragraphs
+const messageOne = document.querySelector('#message-error')
+const messageTwo = document.querySelector('#message-success')
+
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault() //prevents default behavior to refresh the browser on formsubmit
+    
+    const location = searchBox.value
+    //console.log(location)
+
+    messageOne.textContent = "LOADING...."
+    messageTwo.textContent = ''
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
     response.json().then(({error, fullname, forecast} = {}) => {
         //runs when the json has arrived and parsed
         if(error){
-            console.log(error)
+            messageOne.textContent = error
+            messageTwo.textContent = ''
         }else{
-            console.log(fullname)
-            console.log(forecast)
+            messageOne.textContent = fullname+forecast
+            messageTwo.textContent = ''
         }
     })
+})
 })
